d0f0e97719c5be0c4419e058de17de6c598493e4
Nona <81672054+Naru-hub@users.noreply.github.com>
2022-02-22 16:48:14 +0900

    SelectBox リファクタリング

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import { useState } from "react";
 import "./styles.css";
 
+//セレクトボックスにセットしたい項目の配列
+const values = [
+  { id: 1, item: "HTML" },
+  { id: 2, item: "CSS" },
+  { id: 3, item: "JavaScript" }
+];
+
+//map()で配列valuesから要素を1つずつ取り出しvalueを受け取る
+const SelectItems = values.map((value) => {
+  return (
+    <option value={value.item} key={value.id}>
+      {value.item}
+    </option>
+  );
+});
+
 const InputSelectBox = () => {
-  const [selectedValue, setSelectedValue] = useState("HTML");
+  const [selectedValue, setSelectedValue] = useState(values[0]["item"]);
 
   //selectedValueを更新するhandleChange関数を宣言
   const handleChange = (e) => {
@@ -12,14 +28,13 @@ const InputSelectBox = () => {
   return (
     <div className="App">
       <p>
+        {/* onChangeでselectedValueの状態が更新され画面上の表示が変化 */}
         現在選択されている値 :<b>{selectedValue}</b>
       </p>
 
       {/* onChangeでselectedValueの状態がvalueの値に更新される */}
       <select value={selectedValue} onChange={handleChange}>
-        <option value="HTML">HTML</option>
-        <option value="CSS">CSS</option>
-        <option value="JavaScript">JavaScript</option>
+        {SelectItems}
       </select>
     </div>
   );
